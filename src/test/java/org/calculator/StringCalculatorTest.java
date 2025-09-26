d310387438931fa9242da272bc1627ab30ac4ffe
Shivansh Dubey <shivanshd4@gmail.com>
2025-09-26 14:18:10 +0530

    test: add single-number case

--- src/test/java/org/calculator/StringCalculatorTest.java
+++ src/test/java/org/calculator/StringCalculatorTest.java
@@ -7,9 +7,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StringCalculatorTest {
     private final StringCalculator calculator = new StringCalculator();
 
-    // TODO:  create an add method in StringCalculator Class.
     @Test
     void emptyStringTest() {
         assertEquals(0,calculator.add(""));
+    }
+
+    // TODO handle addition of a single no string
+    void addSingleNumberTest() {
+        assertEquals(1,calculator.add("1"));
     }
 }
