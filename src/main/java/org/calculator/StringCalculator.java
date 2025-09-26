e19ac79f5cdab9075dc6cf504a278e1c8168ac3e
Shivansh Dubey <shivanshd4@gmail.com>
2025-09-26 14:15:29 +0530

    impl: return 0 for empty string

--- src/main/java/org/calculator/StringCalculator.java
+++ src/main/java/org/calculator/StringCalculator.java
@@ -1,4 +1,12 @@
 package org.calculator;
 
 public class StringCalculator {
+
+
+    public int add (String numbers) {
+        if (numbers == null || numbers.isEmpty()) {
+            return 0;
+        }
+        throw new UnsupportedOperationException("Implementation Pending");
+    }
 }
